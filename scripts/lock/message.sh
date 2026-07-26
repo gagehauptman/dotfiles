27cb7aaa8b475d42b876b9f94f7b7c3876d479c5
Gage Hauptman <ghauptman@proton.me>
2026-07-26 05:36:53 -0600

    orientation-agnostic hyprlock layout

--- scripts/lock/message.sh
+++ scripts/lock/message.sh
@@ -1,14 +1,15 @@
-#!/bin/sh
-
 #!/bin/bash
+# hyprlock greeting. Weight is pango markup since hyprlock has no font_weight;
+# keep it matched to the clock label in hyprlock.conf.
 
 current_hour=$(date +%H)
 
 if (( current_hour >= 0 && current_hour < 12 )); then
-    echo "good morning, $USER"
+    greeting="good morning, $USER"
 elif (( current_hour >= 12 && current_hour < 18 )); then
-    echo "good afternoon, $USER"
+    greeting="good afternoon, $USER"
 else
-    echo "good evening, $USER"
+    greeting="good evening, $USER"
 fi
 
+printf "<span weight='medium'>%s</span>\n" "$greeting"
